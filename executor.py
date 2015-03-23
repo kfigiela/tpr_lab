c064a74c094e78fdc4067498b7ebe15cb6d2daaa
Kamil Figiela <kamil.figiela@gmail.com>
2015-03-23 08:24:51 +0100

    Scalarm executor

---
+++ executor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python2.6
+import json, sys, os, commands
+ 
+data = None
+with open("input.json") as json_data:
+    data = json.load(json_data)
+ 
+niters = data['niters']
+nprocs = data['nprocs']
+ 
+dir = os.path.dirname(os.path.realpath(__file__))
+ 
+cmd = "module add openmpi/1.6.4-gnu-4.7.2-ib && (mpiexec -np %s %s/pi_mpi %s > output.json)" % (nprocs, dir, niters)
+
+status, output = commands.getstatusoutput(cmd)
+print output
